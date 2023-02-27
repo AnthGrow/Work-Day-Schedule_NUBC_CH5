bbda0ca118f23f108b96c2ad8b24920c02b4439d
AnthGrow <122587083+AnthGrow@users.noreply.github.com>
2023-02-26 20:36:15 -0600

    tried two different approaches to color coding and reading and parsing time - now have to figure out how to test if it's working - either changing the code or changing the time zone on my computer :/

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -24,15 +24,95 @@ $(document).ready(function() {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
 
-  const saveBtns = document.querySelectorAll(".saveBtn");
+  // const saveBtns = document.querySelectorAll(".saveBtn");
 
-  saveBtns.forEach(function(btn) {
-    btn.addEventListener("click", function() {
-      console.log("button clicked");
-    });
-  });
+  // saveBtns.forEach(function(btn) {
+  //   btn.addEventListener("click", function() {
+  //     console.log("button clicked");
+  //   });
+  // });
 
-  
+ // Add click event listener to all save buttons
+ $(".saveBtn").on("click", function() {
+  // Get the value of the textarea element
+  var text = $(this).siblings(".description").val();
+  // Get the ID of the parent row element
+  var id = $(this).parent().attr("id");
+  // Save the text to local storage using the ID as the key
+  localStorage.setItem(id, text);
+});
+
+// Load saved data from local storage
+$(".time-block").each(function() {
+  // Get the ID of the row element
+  var id = $(this).attr("id");
+  // Get the saved text from local storage using the ID as the key
+  var text = localStorage.getItem(id);
+  // If there is saved text, update the textarea element with it
+  if (text !== null) {
+    $(this).find(".description").val(text);
+  }
+});
+ 
+// Get the current hour using day.js
+var currentHour = dayjs().hour();
+
+// Loop through each element with the class "time-block"
+$(".time-block").each(function() {
+  // Get the hour of the current element from its ID
+  var elementHour = parseInt($(this).attr("id").split("-")[1]);
+
+  // Compare the current hour with the element hour
+  if (currentHour < elementHour) {
+    $(this).addClass("future");
+  } else if (currentHour === elementHour) {
+    $(this).addClass("present");
+  } else {
+    $(this).addClass("past");
+  }
+});
+
+// Get the current hour using day.js
+var currentHour = dayjs().hour();
+
+// loop over the hours from 8AM to 5PM
+for (var hour = 8; hour <= 17; hour++) {
+  // create a variable for the container div with the corresponding ID
+  var container = $('#hour-' + hour);
+
+  // check if the current hour is greater than the hour for this container
+  if (currentHour > hour) {
+    container.addClass('past');
+  }
+  // check if the current hour is equal to the hour for this container
+  else if (currentHour === hour) {
+    container.addClass('present');
+  }
+  // otherwise, the current hour must be less than the hour for this container
+  else {
+    container.addClass('future');
+  }
+}
+
+
+// // Select the container element using its ID
+// var container = $('#hour-11');
+
+// // Get the hour value from the container's ID
+// var containerHour = parseInt(container.attr('id').split('-')[1]);
+
+// // Compare the current hour with the container hour
+// if (containerHour < currentHour) {
+//   // If the container hour is in the past, add the 'past' class
+//   container.addClass('past');
+// } else if (containerHour === currentHour) {
+//   // If the container hour is the same as the current hour, add the 'present' class
+//   container.addClass('present');
+// } else {
+//   // If the container hour is in the future, add the 'future' class
+//   container.addClass('future');
+// }
+
 
   //
   // TODO: Add code to apply the past, present, or future class to each time
